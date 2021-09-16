5669710d803fefbfe03a564c0f128f6897299716
Steve Hand <steve_hand@ultimatesoftware.com>
2021-09-16 10:04:53 -0500

    Updated README and added cache report script

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # cache_wrangling
 An exploration of how to view the MongoDB cache contents and act on what you see
+
+The 'cache' being referred to here is not the query plan cache, but, instead the RAM set aside by the WiredTiger storage engine to cache documents and indexes for faster access. In short, despite the improvements in storage speed over time, RAM is faster than storage.  
+
+The [FAQ MongoDB Diagnostics](https://docs.mongodb.com/manual/faq/diagnostics/) goes into some depth about what the cache is, how to view it, and its function to handle the [working set](https://en.wikipedia.org/wiki/Working_set).  Certainly, MongoDB users ought to be cognizant of how work the DBMS is doing to read data blocks into and evict out of the cache.
+
+If a block MongoDB needs is not in RAM (a cache fault), it has to read it from storage. The more your DBMS is doing that, the slower the overall system performance. This situation is also described as having a working set that is larger than real RAM (as opposed to virtual RAM). As the FAQ states, this is not necessarily something to be concerned about. 
+
+However, it is not clear what to do if you find that your system is overly busy in its cache management. One set of mitigations includes ensuring that queries use indexes and the information model is designed optimally. But, it is still possible, even with optimized queries (and other DB ops), that your system is performing slower than expectations because it's overly busy managing its cache.
+
+What steps would need to be taken to determine what databases, collections, and indexes are being swapped in and out of cache the most often? Knowing this would help to direct your efforts to the parts of your information system that need the most attention. It may not be obvious which queries are slow because of cache faults. The performance of even the most optimal queries/DB ops can be affected by the other, most costly operations.
+
+The work in this repo seeks to explain methods and techniques for using cache metrics to solve performance problems.
+
+
---
+++ src/mongo_shell/cache_report.js
@@ -0,0 +1,222 @@
+// Steve Hand 2021-JUN-02
+// Report on the bytes in cache for all collections and indexes in a databases
+// HOWTO derived from keyhole
+// https://github.com/simagix/keyhole/blob/4eee388b0602dfbf4819909714151ca88f61ed03/mdb/wt_cache.go#L56
+//
+// See "Memory Diagnostics for Wired Tiger Storage Engine"
+// https://docs.mongodb.com/v4.0/faq/diagnostics/#memory-diagnostics-for-the-wiredtiger-storage-engine
+// https://docs.mongodb.com/manual/faq/diagnostics/#memory-diagnostics-for-the-wiredtiger-storage-engine
+//
+// In general,
+//  index data is somewhat compressed in the cache (using inddex prefix compression)
+//  collection data is NOT compressed in the cache
+
+/**
+ * Right pad the provided string with the specified character
+ * @param width
+ * @param string
+ * @return {*}
+ */
+function pad(width, string) {
+	// assert(typeof width === "number", 'width arg must be number');
+	// assert(typeof string === "string", 'string arg must be string');
+	var padded = "";
+	if(width >= string.length) {
+		padded = string.padStart(width);
+	}
+	else {
+		padded = string.substring(0, width - 3) + '...';
+	}
+	return padded;
+}
+
+/**
+ * @param n number to format
+ * @return formatted string
+ */
+function niceNum(n) {
+	assert(typeof n === "number", 'n arg must be number');
+	return n.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")
+}
+
+/**
+ * @param collName collection name
+ * @return returns an array object objects
+ *    each with indxName and cachedBytes
+ */
+ function getIndexCachedArray(collName) {
+ 	var indexCachedArray = [];
+
+ 	try {
+ 		var	indexStats = db[collName].stats({indexDetails:true});
+ 	}
+ 	catch(e) {
+ 		if(e instanceof TypeError) { // may be special collection or a view
+ 			return indexCachedArray;
+ 		}
+ 	}
+ 	// See https://docs.mongodb.com/v4.0/reference/method/db.collection.stats/
+   if(typeof indexStats['indexDetails'] !== 'undefined') {
+     Object.keys(indexStats.indexSizes).forEach(
+       function(d) {
+ 				var bcic = indexStats['indexDetails'][d]['cache']["bytes currently in the cache"]
+ 				indexCachedArray.push({idxName: d, cachedBytes:bcic});
+       }
+     )
+ 		indexCachedArray.sort(function(a,b) {return a.cachedBytes > b.cachedBytes})
+ 	}
+ 	return indexCachedArray;
+ }
+
+
+/**
+ * @param collName collection name
+ * @return returns the size of the collection's cached data
+ */
+function getCollCached(collName) {
+	try {
+		var stats = db[collName].stats();
+		if(typeof stats['wiredTiger'] !== 'undefined') {
+			return stats['wiredTiger']['cache']["bytes currently in the cache"]
+		}
+		else {
+			return 0
+		}
+	}
+	catch (e) {
+		if(e instanceof TypeError) { // may be special collection or a view
+			return 0;
+		}
+		else {
+			throw e;
+		}
+	}
+}
+
+/**
+ * @param num number that is to be made 'human readable' and formatted
+ * @return returns a formatted string
+ */
+function humanReadableNumber(num) {
+	var aMB = Math.pow(1024, 2);
+	var aGB = Math.pow(1024, 3);
+	var rtnNum = "0";
+	if(num > aGB) {
+		rtnNum = niceNum(parseFloat((num/aGB).toFixed(2))) + " gb";
+	}
+	else if(num > aMB) {
+		rtnNum = niceNum(parseFloat((num/aMB).toFixed(2))) + " mb";
+	}
+	else {
+		rtnNum = niceNum(num) + "  b";
+	}
+	return rtnNum;
+}
+
+/**
+ * @param totalCachedNum total cached data size
+ * @param cachedNumber collection or index cached data size
+ * @return string containing the percent
+ */
+function cachedPercentString(totalCachedNum, cachedNum) {
+	var c = cachedNum / totalCachedNum * 100;
+	var rtnStr = "";
+	if(c > 0.01) {
+		rtnStr = c.toFixed(2).toString();
+	}
+	else {
+		rtnStr = "lt .01";
+	}
+	return rtnStr;
+}
+
+/**
+ * Produces a cxache report of for all collections and indexes in
+ * the specified database
+ * @param dbName name of database
+ */
+function dbCacheReport(dbName) {
+	db = db.getSiblingDB(dbName);
+	print(`DB name:\t${db}`)
+	// need clusterMonitor role to get the total size of cache data (serverStatus privlege)
+	var totalCacheSize = db.serverStatus()['wiredTiger']['cache']["bytes currently in the cache"];
+	print(`cached data: \t`+ humanReadableNumber(totalCacheSize));
+	print();
+
+	var collNameSize = 20;
+	var collNameHeader = pad(collNameSize, "COLL NAME")
+	var collCachedSize = 10;
+	var collCachedHeader = pad(collCachedSize, "CACHED")
+	var collCachedPercentSize = 6;
+	var collCachedPHeader = pad(collCachedPercentSize, "%")
+	var indexNameSize = 20
+	var indexNameHeader = pad(indexNameSize, "INDEX NAME")
+	var indexCachedSize = 10;
+	var indexCachedHeader = pad(indexCachedSize, "CACHED")
+	var indexCachedPercentSize = 6;
+	var indexCachedPHeader = pad(indexCachedPercentSize, "%")
+	print(`${collNameHeader} ${collCachedHeader} ${collCachedPHeader} ${indexNameHeader} ${indexCachedHeader} ${indexCachedPHeader} `);
+
+	var collNames = db.getCollectionNames();
+	var collCached = [];
+	for(let collName of collNames){
+			var cc = getCollCached(collName)
+			collCached.push({cn: collName, cached: cc});
+		}
+
+	collCached.sort(function(a,b) {return a.cached > b.cached});
+	for(let x of collCached) {
+			var indexCached = getIndexCachedArray(x.cn);
+			for(let i = 0; i < indexCached.length; i++)	{
+				if(i === 0) {
+					print(pad(collNameSize, x.cn) + ' '
+					  + pad(collCachedSize, humanReadableNumber(x.cached)) + ' '
+						+ pad(collCachedPercentSize, cachedPercentString(totalCacheSize, x.cached)) + ' '
+						+ pad(indexNameSize, indexCached[i].idxName) + ' '
+						+ pad(indexCachedSize, humanReadableNumber(indexCached[i].cachedBytes)) + ' '
+						+ pad(indexCachedPercentSize, cachedPercentString(totalCacheSize, indexCached[i].cachedBytes))
+					);
+				}
+				else {
+					print(pad(collNameSize, ' -') + ' '
+						+ pad(collCachedSize, ' ') + ' '
+						+ pad(collCachedPercentSize, ' ') + ' '
+						+ pad(indexNameSize, indexCached[i].idxName) + ' '
+						+ pad(indexCachedSize, humanReadableNumber(indexCached[i].cachedBytes)) + ' '
+						+ pad(indexCachedPercentSize, cachedPercentString(totalCacheSize, indexCached[i].cachedBytes))
+					);
+				}
+			}
+		}
+}
+
+/**
+ * produce cache usage report
+ * @param scope "current" database or "all" databases
+ */
+function cacheReport(scope="current") {
+	var dt = Date();
+	print(`date:\t\t${dt}`);
+	switch(scope) {
+		case "current":
+			var dbName = db;
+			dbCacheReport(dbName);
+			break;
+
+		case "all":
+			var adminDBs = ['admin','config','local'];
+			db.adminCommand('listDatabases').databases.forEach(function(d) {
+				if(! adminDBs.includes(d.name)){
+					dbCacheReport(d.name);
+					print("**********");
+				}
+			})
+			break;
+
+		default:
+			print('function takes "current" database or "all" databases');
+	}
+}
+
+// main
+cacheReport();
